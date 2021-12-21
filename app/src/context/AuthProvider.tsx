7b743313716e7b10a15f4c55c9d54fa4579acbca
JuMaLaB <julien.malaboeuf@gmail.com>
2021-12-21 17:22:50 +0100

    use typescript with useState useContext

--- app/src/context/AuthProvider.tsx
+++ app/src/context/AuthProvider.tsx
@@ -1,20 +1,52 @@
 import React from "react";
 
-export const AuthContext = React.createContext();
+interface AuthContextValues {
+  authInfo: AuthInfo;
+  isAuthenticated: boolean;
+  // setAuthInfo: React.Dispatch<React.SetStateAction<AuthInfo>>;
+  setAuthInfo: (authInfo: AuthInfo) => void;
+  isAdmin: boolean;
+}
+
+export const AuthContext = React.createContext<undefined | AuthContextValues>(undefined);
 const Provider = AuthContext.Provider;
 
-export function AuthProvider({ children }) {
-  const [authInfo, setAuthInfo] = React.useState({
+interface Props {
+  children: React.ReactChild;
+}
+interface UserData {
+  role: "USER" | "ADMIN";
+}
+interface AuthInfo {
+  userData: UserData | null;
+}
+
+export function AuthProvider({ children }: Props) {
+  const [authInfo, setAuthInfo] = React.useState<AuthInfo>({
     userData: null,
   });
 
-  const isAuthenticated = authInfo.userData;
+  const isAuthenticated = authInfo.userData !== null;
 
   const isAdmin = authInfo.userData?.role === "ADMIN";
 
+  function handleAuthInfo(authInfo: AuthInfo) {
+    setAuthInfo(authInfo);
+  };
+
   return (
-    <Provider value={{ authInfo, isAuthenticated, setAuthInfo, isAdmin }}>
+    <Provider value={{ authInfo, isAuthenticated, setAuthInfo: handleAuthInfo, isAdmin }}>
       {children}
     </Provider>
   );
+}
+
+export function useAuthContext() {
+  const context = React.useContext(AuthContext);
+
+  if (context === undefined) {
+    // handle
+    throw new Error("useAuthContext should be used within an AuthProvider.");
+  }
+  return context;
 }

54f15e60a7a624a80eeb5da264712cee232dee37
JuMaLaB <julien.malaboeuf@gmail.com>
2021-12-21 17:16:35 +0100

    add a function with props

--- app/src/pages/conference/Navigation.tsx
+++ app/src/pages/conference/Navigation.tsx
@@ -1,7 +1,29 @@
 import * as React from "react";
 import { useRouteMatch, Link } from "react-router-dom";
 
+
+function NavigationLink(props: { label: string, to: string }) {
+  return (
+    <Link
+      style={{
+        fontSize: 56,
+        border: "solid 1px white",
+        borderRadius: 20,
+        margin: 20,
+        padding: 20,
+        textAlign: "center",
+        backgroundColor: "#0D1424",
+        textTransform: "capitalize",
+      }}
+      to={props.to}
+    >
+      {props.label.toLowerCase()}
+    </Link>
+  );
+}
+
 export function Navigation() {
+
   const { url } = useRouteMatch();
 
   // throw Error("Fuck this is an Error !!!");
@@ -20,50 +42,9 @@ export function Navigation() {
               justifyContent: "center",
             }}
           >
-            <Link
-              style={{
-                fontSize: 56,
-                border: "solid 1px white",
-                borderRadius: 20,
-                margin: 20,
-                padding: 20,
-                textAlign: "center",
-                backgroundColor: "#0D1424",
-                textTransform: "capitalize",
-              }}
-            >
-              View speakers
-            </Link>
-            <Link
-              style={{
-                fontSize: 56,
-                border: "solid 1px white",
-                borderRadius: 20,
-                margin: 20,
-                padding: 20,
-                textAlign: "center",
-                backgroundColor: "#0D1424",
-                textTransform: "capitalize",
-              }}
-              to={`${url}/sessions`}
-            >
-              View Sessions
-            </Link>
-            <Link
-              style={{
-                fontSize: 56,
-                border: "solid 1px white",
-                borderRadius: 20,
-                margin: 20,
-                padding: 20,
-                textAlign: "center",
-                backgroundColor: "#0D1424",
-                textTransform: "capitalize",
-              }}
-              to={`${url}/about`}
-            >
-              about us
-            </Link>
+            <NavigationLink label="View speakers" to={`${url}/speakers`} />
+            <NavigationLink label="View Sessions" to={`${url}/sessions`} />
+            <NavigationLink label="about us" to={`${url}/about`} />
           </div>
         </div>
       </div>
